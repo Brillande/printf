f41ec9db10752c1a2346a77fbcddb106a5d3e020
Ellande Medina Seoane <jelland90@gmail.com>
2023-07-18 13:48:55 +0200

    terminado

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 <<<<<<< HEAD
 creo haberlo acabado falta entregarlo y confirmarlo
 =======
-creo haberlo acabado falta entregarlo, corregir norminette y confirmarlo
+proyecto finalizado sin bonus.
 >>>>>>> 9bef5baec5b39a55cf97218a5a9fb4f1726341e8
--- ft_printf.h
+++ ft_printf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emedina- <emedina-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: emedina- <emedina-@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 19:21:06 by emedina-          #+#    #+#             */
-/*   Updated: 2023/07/04 21:09:43 by emedina-         ###   ########.fr       */
+/*   Updated: 2023/07/07 00:36:00 by emedina-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,20 @@
 # define HEX_UPP_BASE "0123456789ABCDEF"
 # define HEX_LOW_BASE "0123456789abcdef"
 
-# include <stdio.h>
-# include <stdarg.h>
 # include <limits.h>
+# include <stdarg.h>
+# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 
-int     ft_printf(char const *str, ...);
-void    ft_putchar_counter(char c, size_t *counter);
-void    ft_putunsignedint_counter(unsigned int n, size_t *counter);
-void    ft_putptr_counter(void *ptr, size_t *counter);
-void    ft_putstr_counter(char *str, size_t *counter);
-void    ft_putnbr_counter(int n, size_t *counter);
-size_t  ft_strlen(const char *s);
-size_t  ft_len(unsigned long long n, char *base);
-char    *ft_aux(unsigned long long n, char *base);
-void    ft_puthex_counter(unsigned int n, size_t *counter, char *base);
+int		ft_printf(char const *str, ...);
+void	ft_putchar_counter(char c, size_t *counter);
+void	ft_putunsignedint_counter(unsigned int n, size_t *counter);
+void	ft_putptr_counter(void *ptr, size_t *counter);
+void	ft_putstr_counter(char *str, size_t *counter);
+void	ft_putnbr_counter(int n, size_t *counter);
+size_t	ft_strlen(const char *s);
+size_t	ft_len(unsigned long long n, char *base);
+char	*ft_aux(unsigned long long n, char *base);
+void	ft_puthex_counter(unsigned int n, size_t *counter, char *base);
 #endif
